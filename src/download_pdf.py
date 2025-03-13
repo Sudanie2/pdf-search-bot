9a2d36bb12b6f1e7b4c183a0d5da3ddabd153cb8
Sudanie2 <sakamoto.tsubasa.5n@kyoto-u.ac.jp>
2025-03-13 15:10:32 +0900

    Update download_pdf.py

--- src/download_pdf.py
+++ src/download_pdf.py
@@ -1,1 +1,14 @@
+from google.cloud import storage
 
+BUCKET_NAME = "your-bucket-name"
+PDF_PATH = "documents/sample.pdf"
+
+def download_pdf():
+    """GCSからPDFをダウンロード"""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(PDF_PATH)
+    
+    blob.download_to_filename("../data/temp.pdf")
+    print(f"{PDF_PATH} をダウンロードしました")
+    return "../data/temp.pdf"
